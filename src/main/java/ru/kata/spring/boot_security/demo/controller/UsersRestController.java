8bf7866a22211153d0a8c277c7d3c3571243c7f7
Andrey <147320074+ilupat@users.noreply.github.com>
2023-12-28 14:38:39 +0300

    fix

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UsersRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UsersRestController.java
@@ -1,21 +1,19 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-import javassist.NotFoundException;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import ru.kata.spring.boot_security.demo.dao.UserRepository;
 import ru.kata.spring.boot_security.demo.dto.UserDTO;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
+import java.security.Principal;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -25,13 +23,40 @@ import java.util.Set;
 public class UsersRestController {
     private final UserService userService;
     private final RoleService roleService;
+    private final UserRepository userRepository;
+
 
     @Autowired
-    public UsersRestController(UserService userService, RoleService roleService) {
+    public UsersRestController(UserService userService, RoleService roleService, UserRepository userRepository) {
         this.userService = userService;
         this.roleService = roleService;
+        this.userRepository = userRepository;
     }
 
+    @GetMapping("login")
+    public ModelAndView login() {
+        return new ModelAndView("login");
+    }
+    @GetMapping("admin")
+    public ModelAndView index(Principal principal) {
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("user", userRepository.findByUsername(principal.getName()));
+        return modelAndView;
+    }
+
+    @GetMapping("userPage")
+    public ModelAndView forUser(Principal principal) {
+        ModelAndView modelAndView = new ModelAndView("userPage");
+        User user = userRepository.findByUsername(principal.getName());
+        modelAndView.addObject("user", user);
+        return modelAndView;
+    }
+
+    @GetMapping("getUser")
+    public ResponseEntity<User> getUserC(Principal principal) {
+        User user = userRepository.findByUsername(principal.getName());
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
     @GetMapping("admin/list")
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = userService.getAllUsers();
